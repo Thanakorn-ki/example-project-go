8ea041068a9657499a21234e6474f1bc9da949a4
Thanakorn.K <potook_02@hotmail.com>
2018-07-28 21:59:25 +0700

    change get auth

--- middleware.go
+++ middleware.go
@@ -6,7 +6,7 @@ import (
 
 func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		if c.Request().Header["Authorization"][0] == "551122" {
+		if c.Request().Header.Get("Authorization") == "551122" {
 			return next(c)
 		} else {
 			return echo.NewHTTPError(401, "Please provide valid token")
