1b4f085a15d07c5ac51c0bb33622d58c3dcf31f7
Thanakorn.K <potook_02@hotmail.com>
2018-07-31 11:58:07 +0700

    git change logic return error

--- middleware.go
+++ middleware.go
@@ -8,8 +8,7 @@ func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if c.Request().Header.Get("Authorization") == "551122" {
 			return next(c)
-		} else {
-			return echo.NewHTTPError(401, "Please provide valid token")
 		}
+		return echo.NewHTTPError(401, "Please provide valid token")
 	}
 }
